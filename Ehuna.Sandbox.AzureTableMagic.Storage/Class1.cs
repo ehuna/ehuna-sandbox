3ccbcfe7dd2f2675a5a5a11088eca8b0e517d144
Emmanuel Huna <hero@stealthlab.net>
2014-07-24 11:21:37 -0700

    Added a Storage library

---
+++ Ehuna.Sandbox.AzureTableMagic.Storage/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ehuna.Sandbox.AzureTableMagic.Storage
+{
+    public class Class1
+    {
+    }
+}
