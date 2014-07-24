db1ff0a2cddb3e5c68f60c3127e81695ff3630b7
Emmanuel Huna <hero@stealthlab.net>
2014-07-24 11:45:20 -0700

    Added reference to Azure Storage SDK and setup nuget

---
+++ Azure-Table-Magic/Ehuna.Sandbox.AzureTableMagic.Storage/Table/AzureTableRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+//using Microsoft.WindowsAzure.Storage;
+//using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ehuna.Sandbox.AzureTableMagic.Storage.Table
+{
+    public class AzureTableRepository
+    {
+    }
+}
